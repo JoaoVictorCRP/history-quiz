4cef88424fb6eb3ea5bc39c68a28b375fcbff7cc
Joao Victor <joaovictorxl120@gmail.com>
2023-11-02 10:19:21 -0300

    Design improved, sounds added

--- _assets/quiz.js
+++ _assets/quiz.js
@@ -1,8 +1,8 @@
 const questions = [
     {
-        question: "Quem foi o 3º Presidente do Brasil?",
-        options: ["Getúlio Vargas","Prudente de Moraes", "Juscelino Kubitschek", "Deodoro da Fonseca"],
-        correct: "Prudente de Moraes"
+        question: "Qual evento histórico marcou o início da Primeira Guerra Mundial?",
+        options: ["Assassinato do Arquiduque Francisco Ferdinando da Áustria","Bomba atômica em Hiroshima e Nagasaki", "Invasão Alemã na Itália", "Revolução Russa"],
+        correct: "Assassinato do Arquiduque Francisco Ferdinando da Áustria"
     },
     {
         question: "Qual imperador romano nomeou o próprio cavalo ao senado?",
@@ -57,8 +57,11 @@ let d = document.querySelector('#d');
 let buttons = document.getElementsByTagName('button')
 let title = document.querySelector('#title');
 let img = document.querySelector('#image');
+let correctSound = document.getElementById("correctSound");
+let wrongSound = document.getElementById("wrongSound");
 let i = 0;
 let correct = 0;
+
 
 
 function next () {   //Função para passar para a próxima pergunta
@@ -66,19 +69,17 @@ function next () {   //Função para passar para a próxima pergunta
     if(i < questions.length) {
         title.innerText = questions[i].question;
         img.innerHTML = `<img  src="./_assets/_imagens/${i}.jpg">`
-        a.innerText = questions[i].options[0];
-        b.innerText = questions[i].options[1];
-        c.innerText = questions[i].options[2];
-        d.innerText = questions[i].options[3];
+        a.innerText = `${questions[i].options[0]}`;
+        b.innerText = `${questions[i].options[1]}`;
+        c.innerText = `${questions[i].options[2]}`;
+        d.innerText = `${questions[i].options[3]}`;
         i++;
 
         for (let button of buttons){
             button.removeEventListener('click', checkAnswer); //Removendo EventListeners
-        }
-
-        for (let button of buttons) {
             button.addEventListener('click', checkAnswer); // Repondo EventListeners
         }
+        console.log(correct)
     } else {
         alert(`Você acertou um total de ${correct} Perguntas, Muito bem!`)
         //Fim do questionário
@@ -88,11 +89,12 @@ function next () {   //Função para passar para a próxima pergunta
 function checkAnswer() { // checar resposta e repetir a função next
 
     let selectedAnswer = this.innerText; // Obtém a resposta selecionada
-    let correctAnswer = questions[i - 1].correct;
+    let correctAnswer = questions[i-1].correct;
 
     if (selectedAnswer === correctAnswer) {
         correct++;
-    }
+        correctSound.play()
+    } else {     wrongSound.play() }
 
     next();
 }
